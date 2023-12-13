335ad39ceec99554b745d08599441d83a1bcb5c8
jakbot <lijinze46@gmail.com>
2023-12-13 21:23:41 +1300

    Updates

---
+++ precache-manifest.02af6837ca12c0a06fec07712333263f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0148992dbbff4f2e31495fd0a47de186",
+    "url": "/react-Project-Management-Application/index.html"
+  },
+  {
+    "revision": "9ac69127356ac7e7837d",
+    "url": "/react-Project-Management-Application/static/css/main.be73b791.chunk.css"
+  },
+  {
+    "revision": "d30c789d3ded2d6c497b",
+    "url": "/react-Project-Management-Application/static/js/2.af95dedf.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/react-Project-Management-Application/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "9ac69127356ac7e7837d",
+    "url": "/react-Project-Management-Application/static/js/main.de39f3f2.chunk.js"
+  },
+  {
+    "revision": "eae76aa90e5c1276b936",
+    "url": "/react-Project-Management-Application/static/js/runtime~main.b58f29c4.js"
+  }
+]);
